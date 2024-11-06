d5dbe09f00a15af477f75bc99e65a36637b6350f
Dan Buchholz <dbuchholz30@gmail.com>
2024-11-06 11:10:34 -0500

    feat: cbor

--- packages/fvm/src/utils/index.ts
+++ packages/fvm/src/utils/index.ts
@@ -1,3 +1,4 @@
-export * from "./base32.js";
+export * as base32 from "./base32.js";
+export * as cbor from "./cbor.js";
 export * from "./convert.js";
-export * from "./leb128/index.js";
+export * as leb128 from "./leb128/index.js";
