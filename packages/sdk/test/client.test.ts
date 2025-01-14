75af5c4fabeeab33a8deddedb013953b23c5a545
Dan Buchholz <dbuchholz30@gmail.com>
2025-01-13 16:38:03 -0800

    chore: add node types & adjust 'assert' imports

--- packages/sdk/test/client.test.ts
+++ packages/sdk/test/client.test.ts
@@ -1,4 +1,4 @@
-import { strictEqual } from "assert";
+import { strictEqual } from "node:assert";
 import { describe, it } from "mocha";
 import { localnet, testnet } from "../src/chains.js";
 import {
