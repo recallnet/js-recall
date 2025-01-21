f69bdb9d11ab7a9930ec247f1ef8f09fbe88ead9
Aaron Sutula <asutula@users.noreply.github.com>
2025-01-21 11:58:40 -0600

    main page cleanup

--- apps/studio/app/page.tsx
+++ apps/studio/app/page.tsx
@@ -1,13 +1,11 @@
 import { Button } from "@recall/ui/components/button";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
+    <div className="flex items-center justify-center">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
-        <ConnectButton />
       </div>
     </div>
   );
