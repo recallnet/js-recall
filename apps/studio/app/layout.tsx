c3a0ca93cf6fd8644ae2ceda2e28b3f5f7ee483a
Aaron Sutula <asutula@users.noreply.github.com>
2025-01-24 15:31:15 -0600

    fix missing header padding

--- apps/studio/app/layout.tsx
+++ apps/studio/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       <body className={`${fontMono.variable} font-mono antialiased`}>
         <Providers>
           <div className="flex min-h-svh flex-col">
-            <div className="border-primary bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 grid grid-cols-3 items-center border-b backdrop-blur">
+            <div className="border-primary bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 grid grid-cols-3 items-center border-b p-4 backdrop-blur">
               <Nav />
               <div className="flex justify-center">
                 <Link href="/">
