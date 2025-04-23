6660eff3ea0679d017b563aeb459d355cf492590
Rcontre360 <rcontreraspimentel@gmail.com>
2025-04-23 18:47:48 -0400

    feat: added button

--- apps/comps/app/page.tsx
+++ apps/comps/app/page.tsx
@@ -1,21 +1,26 @@
+import {ConnectAndSIWE} from "@/components/siwe";
 import {
   Avatar,
   AvatarFallback,
   AvatarImage,
 } from "@recallnet/ui2/components/avatar";
-import { Button } from "@recallnet/ui2/components/button";
-import { Button as ShadcnButton } from "@recallnet/ui2/components/shadcn/button";
+import {Button} from "@recallnet/ui2/components/button";
+import {Button as ShadcnButton} from "@recallnet/ui2/components/shadcn/button";
 
 export default function Page() {
   return (
-    <div className="flex flex-col items-center gap-4">
-      <Avatar>
-        <AvatarImage src="https://github.com/recallnet.png" alt="recallnet" />
-        <AvatarFallback>R</AvatarFallback>
-      </Avatar>
-      <Button variant="default">Default button</Button>
-      <Button variant="destructive">Destructive button</Button>
-      <ShadcnButton>Shadcn button</ShadcnButton>
-    </div>
+    <main className="container mx-auto p-4">
+      <div className="flex flex-col items-center gap-4">
+        <Avatar>
+          <AvatarImage src="https://github.com/recallnet.png" alt="recallnet" />
+          <AvatarFallback>R</AvatarFallback>
+        </Avatar>
+        <Button variant="default">Default button</Button>
+        <Button variant="destructive">Destructive button</Button>
+        <ShadcnButton>Shadcn button</ShadcnButton>
+        <ConnectAndSIWE />
+      </div>
+    </main>
   );
 }
+
