7ed56ecd3cf5e59c5e64f043219ddd4a6d1da198
Rcontre360 <rcontreraspimentel@gmail.com>
2025-04-23 18:46:24 -0400

    feat: siwe component

--- apps/comps/components/siwe.tsx
+++ apps/comps/components/siwe.tsx
@@ -1,46 +1,41 @@
 "use client";
 
+import {useCallback, useEffect, useState} from "react";
+import type {Hex} from "viem";
 import axios from "axios";
-import { useAtom } from "jotai";
-import React, { useCallback, useEffect, useState } from "react";
-import type { Hex } from "viem";
-import { createSiweMessage } from "viem/siwe";
-import { useAccount, useConnect, usePublicClient, useSignMessage } from "wagmi";
+import {useAccount, useConnect, usePublicClient, useSignMessage} from "wagmi";
+import {SiweMessage} from "siwe";
+import {cbWalletConnector} from "@/wagmi-config";
+import {Button} from "@recallnet/ui2/components/button";
 
-import { Button } from "@recallnet/ui2/components/shadcn/button";
-
-import { userAtom } from "@/state/atoms";
-import { cbWalletConnector } from "@/wagmi-config";
-
-export const SIWEButton: React.FunctionComponent<
-  React.ComponentProps<typeof Button>
-> = (props) => {
-  const [message, setMessage] = useState<string>("");
+export function ConnectAndSIWE() {
+  const [message, setMessage] = useState<SiweMessage | undefined>(undefined);
   const [signature, setSignature] = useState<Hex | undefined>(undefined);
-  const [, setUser] = useAtom(userAtom);
+  const [valid, setValid] = useState<boolean | undefined>(undefined);
+  const [login, setLoggedIn] = useState<boolean | undefined>(undefined);
 
-  const { signMessage } = useSignMessage({
+  const {signMessage} = useSignMessage({
     mutation: {
       onSuccess: (sig) => setSignature(sig),
     },
   });
 
-  const { connect } = useConnect({
+  const {connect} = useConnect({
     mutation: {
       onSuccess: async (data) => {
-        const { data: res } = await axios<{ nonce: string }>({
-          baseURL: "",
-          method: "get",
-          url: "/api/nonce",
+        const {data: res} = await axios<{nonce: number}>({
+          baseURL: '',
+          method: 'get',
+          url: '/api/nonce',
           headers: {
-            Accept: "application/json",
+            Accept: 'application/json',
           },
-          data: null,
-        });
+          data: null
+        })
 
         const address = data.accounts[0];
         const chainId = data.chainId;
-        const m = createSiweMessage({
+        const m = new SiweMessage({
           domain: document.location.host,
           address,
           chainId,
@@ -50,7 +45,7 @@ export const SIWEButton: React.FunctionComponent<
           nonce: res.nonce,
         });
         setMessage(m);
-        signMessage({ account: address, message: m });
+        signMessage({account: address, message: m.prepareMessage()});
       },
     },
   });
@@ -63,40 +58,41 @@ export const SIWEButton: React.FunctionComponent<
 
     const localValid = await client.verifyMessage({
       address: account.address,
-      message: message,
+      message: message.prepareMessage(),
       signature,
     });
 
+    setValid(localValid);
+
     if (localValid) {
       try {
-        const res = await axios<{ ok: boolean; address: string }>({
-          baseURL: "",
-          method: "post",
-          url: "/api/login",
+        const res = await axios<{ok: boolean, address: string}>({
+          baseURL: '',
+          method: 'post',
+          url: '/api/login',
           headers: {
-            Accept: "application/json",
+            Accept: 'application/json',
           },
-          data: { message, signature },
-        });
+          data: {message, signature}
+        })
 
-        if (res.status >= 200 && res.status < 300)
-          setUser({ address: account.address, loggedIn: true });
+
+        setLoggedIn(true)
       } catch (err) {
         console.error("SIWE login failed:", err);
       }
     }
-  }, [signature, account, client, message, setUser]);
+  }, [signature, account, client, message]);
 
   useEffect(() => {
     checkValid();
-  }, [signature, account, checkValid]);
+  }, [signature, account]);
 
   return (
-    <Button
-      onClick={() => connect({ connector: cbWalletConnector })}
-      {...props}
-    >
-      {props.children}
+    <Button onClick={() => connect({connector: cbWalletConnector})}>
+      {login ? "Logged in" : "Connect + SIWE"}
     </Button>
   );
-};
+}
+
+
