6bfbe2c49f0400efcfc900881837d2e7cf0f9141
ramirotw <ramirovazquez@gmail.com>
2025-04-30 12:41:50 +0000

    fix: utils import

--- packages/ui2/src/components/input.tsx
+++ packages/ui2/src/components/input.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-import { cn } from "../lib/utils.js";
+import { cn } from "@recallnet/ui2/lib/utils";
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
