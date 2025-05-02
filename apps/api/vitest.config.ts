0fd41b7d3957cb2435e905cfd9400d3d4db62c5a
Mark Krasner <mzkrasner@gmail.com>
2025-05-02 13:02:04 -0400

    fix: updates log-reporter to output results in log file

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -4,15 +4,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    reporters: [
-      "verbose",
-      [
-        "json",
-        {
-          outputFile: "./e2e/e2e-server.json",
-        },
-      ],
-    ],
+    reporters: ["verbose", "e2e/utils/log-reporter.ts"],
     workspace: [
       {
         extends: true,
