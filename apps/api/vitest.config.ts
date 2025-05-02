3154d498ddde4b02af30f3f103c6cf9d508f467d
Mark Krasner <70119343+mzkrasner@users.noreply.github.com>
2025-05-02 12:41:59 -0400

    fix: update static results reporting to json format in vitest (#270)
    # Summary
    
    - New Vitest configuration makes the prior reporting output in
    e2e-server.log less helpful
    - Implemented simple verbose json logging in a new gitignored output
    file for local testers to have easy observability into what failed

--- apps/api/vitest.config.ts
+++ apps/api/vitest.config.ts
@@ -1,12 +1,18 @@
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
-import LogReporter from "./e2e/utils/log-reporter.js";
-
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    reporters: ["default", new LogReporter()],
+    reporters: [
+      "verbose",
+      [
+        "json",
+        {
+          outputFile: "./e2e/e2e-server.json",
+        },
+      ],
+    ],
     workspace: [
       {
         extends: true,
