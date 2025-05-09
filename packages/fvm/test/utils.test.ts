a597ca524ed4b87390b139548199bf2d2229d2ce
Dan Buchholz <dbuchholz30@gmail.com>
2025-05-08 19:20:02 -0500

    feat: add base32 helpers & ux tweaks

--- packages/fvm/test/utils.test.ts
+++ packages/fvm/test/utils.test.ts
@@ -3,22 +3,30 @@ import * as u8a from "uint8arrays";
 
 import * as base32 from "../src/utils/base32.js";
 import * as cbor from "../src/utils/cbor.js";
+import { base32ToHex, hexToBase32 } from "../src/utils/convert.js";
 import { Stream } from "../src/utils/leb128/common.js";
 import { signed, unsigned } from "../src/utils/leb128/index.js";
 
 describe("utils", async () => {
   describe("base32", async () => {
     it("should decode base32 to bytes", async () => {
-      let value = "o4gsdesxam4qui3pnd4e54ouglffoqwecfnrdzq".toUpperCase();
+      let value = "o4gsdesxam4qui3pnd4e54ouglffoqwecfnrdzq";
       let bytes = new Uint8Array(base32.decode(value));
       expect(u8a.toString(bytes, "hex")).to.equal(
         "770d21925703390a236f68f84ef1d432ca5742c4115b11e6",
       );
-      value =
-        "4wx2ocgzy2p42egwp5cwiyjhwzz6wt4elwwrrgoujx7ady5oxm7a".toUpperCase();
+      value = "4wx2ocgzy2p42egwp5cwiyjhwzz6wt4elwwrrgoujx7ady5oxm7a";
       bytes = new Uint8Array(base32.decode(value));
       expect(u8a.toString(bytes, "hex")).to.equal(
         "e5afa708d9c69fcd10d67f45646127b673eb4f845dad1899d44dfe01e3aebb3e",
+      );
+    });
+
+    it("should decode base32 to bytes with hex prefix", async () => {
+      const value = "sdjva4z54t65hor2rgwv4goy5zyfx6rdctz55ltjet7gdwdwktoq";
+      const hex = base32ToHex(value);
+      expect(hex).to.equal(
+        "0x90d350733de4fdd3ba3a89ad5e19d8ee705bfa2314f3deae6924fe61d87654dd",
       );
     });
 
@@ -28,8 +36,15 @@ describe("utils", async () => {
         "hex",
       );
       const encoded = base32.encode(value);
+      expect(encoded).to.equal("o4gsdesxam4qui3pnd4e54ouglffoqwecfnrdzq");
+    });
+
+    it("should encode hex to base32", async () => {
+      const value =
+        "0x90d350733de4fdd3ba3a89ad5e19d8ee705bfa2314f3deae6924fe61d87654dd";
+      const encoded = hexToBase32(value);
       expect(encoded).to.equal(
-        "o4gsdesxam4qui3pnd4e54ouglffoqwecfnrdzq".toUpperCase(),
+        "sdjva4z54t65hor2rgwv4goy5zyfx6rdctz55ltjet7gdwdwktoq",
       );
     });
   });
