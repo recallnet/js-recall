d96be7a2aee373ae95647ac0a0a74cdd28e6df6a
Rcontre360 <rcontreraspimentel@gmail.com>
2025-05-13 11:59:28 -0400

    feat: leaderboard new podium

--- apps/comps/app/leaderboard/page.tsx
+++ apps/comps/app/leaderboard/page.tsx
@@ -2,33 +2,16 @@
 
 import React from "react";
 
-import { Leaderboard } from "@/components/leaderboard/index";
-import { RegisterAgentBlock } from "@/components/register-agent-block";
+import {Leaderboard} from "@/components/leaderboard/index";
+import {RegisterAgentBlock} from "@/components/register-agent-block";
 
-import { JoinSwarmSection } from "../../components/join-swarm-section";
-import { NewsletterSection } from "../../components/newsletter-section";
-import { LeaderboardOngoingCompetition } from "../../components/ongoing-competition/leaderboard";
-import { socialLinks } from "../../data/social";
-import { useCompetitions } from "../../hooks/useCompetitions";
-import { CompetitionStatus } from "../../types";
+import {JoinSwarmSection} from "../../components/join-swarm-section";
+import {NewsletterSection} from "../../components/newsletter-section";
+import {socialLinks} from "../../data/social";
 
 export default function LeaderboardPage() {
-  const { data: competitionsData } = useCompetitions({
-    status: CompetitionStatus.Active,
-  });
-
-  // Get current active competition if available
-  const currentCompetition =
-    competitionsData?.competitions && competitionsData.competitions.length > 0
-      ? competitionsData.competitions[0]
-      : null;
-
   return (
     <div className="container mx-auto px-12 py-8">
-      {currentCompetition && (
-        <LeaderboardOngoingCompetition competition={currentCompetition} />
-      )}
-
       <Leaderboard />
 
       <JoinSwarmSection socialLinks={socialLinks} />
