46d06781c7ca67c94f0a492bbf844058c00a5335
Rcontre360 <rcontreraspimentel@gmail.com>
2025-05-19 13:06:48 -0400

    fix: social link cards

--- apps/comps/components/social-link-card.tsx
+++ apps/comps/components/social-link-card.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { ArrowRightIcon } from "@radix-ui/react-icons";
+import {ArrowRightIcon} from "@radix-ui/react-icons";
 import Image from "next/image";
 import React from "react";
 
-import { SocialLink } from "@/data/social";
+import {SocialLink} from "@/data/social";
+import Card from "@/../../packages/ui2/src/components/shadcn/card";
 
 interface SocialLinkCardProps {
   socialLink: SocialLink;
@@ -14,26 +15,28 @@ export const SocialLinkCard: React.FC<SocialLinkCardProps> = ({
   socialLink,
 }) => {
   return (
-    <a
-      href={socialLink.url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="bg-card hover:bg-card flex h-36 flex-col justify-between rounded-md p-6"
-    >
-      <div className="text-primary flex h-8 w-8 items-center justify-center">
-        <Image
-          src={socialLink.icon}
-          alt={`${socialLink.name} icon`}
-          width={24}
-          height={24}
-        />
-      </div>
-      <div className="flex items-center justify-between">
-        <h3 className="text-primary mt-auto text-lg font-bold">
-          {socialLink.name}
-        </h3>
-        <ArrowRightIcon className="text-primary h-5 w-5" />
-      </div>
-    </a>
+    <Card corner="bottom-left" cropSize={30} className={`bg-${socialLink.bg} h-45 flex-col p-6`}>
+      <a
+        href={socialLink.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="h-full w-full flex flex-col justify-between"
+      >
+        <div className="text-primary flex h-8 w-8 items-center justify-center">
+          <Image
+            src={socialLink.icon}
+            alt={`${socialLink.name} icon`}
+            width={24}
+            height={24}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <h3 className="text-primary mt-auto text-lg font-bold">
+            {socialLink.name}
+          </h3>
+          <ArrowRightIcon className="text-primary h-5 w-5" />
+        </div>
+      </a>
+    </Card>
   );
 };
--- apps/comps/data/social.ts
+++ apps/comps/data/social.ts
@@ -3,6 +3,7 @@ export interface SocialLink {
   name: string;
   url: string;
   icon: string;
+  bg?: string;
 }
 
 export const socialLinks: SocialLink[] = [
@@ -11,23 +12,27 @@ export const socialLinks: SocialLink[] = [
     name: "X.COM",
     url: "https://x.com/recallnet",
     icon: "/x-icon.svg",
+    bg: 'card'
   },
   {
     id: "discord",
     name: "DISCORD",
     url: "https://discord.recall.network",
     icon: "/discord-icon.svg",
+    bg: 'blue-500'
   },
   {
     id: "youtube",
     name: "YOUTUBE",
     url: "https://www.youtube.com/@recallnet",
     icon: "/youtube-icon.svg",
+    bg: 'red-500'
   },
   {
     id: "docs",
     name: "DOCS",
     url: "https://docs.recall.network",
     icon: "/docs-icon.svg",
+    bg: 'green-500'
   },
 ];
