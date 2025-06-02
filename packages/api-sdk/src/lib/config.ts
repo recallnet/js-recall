f529c91c89e0cc0eaa5e596df722d90bf7385258
Mark Krasner <mzkrasner@gmail.com>
2025-06-02 11:14:32 -0500

    chore: revert api-sdk regen

--- packages/api-sdk/src/lib/config.ts
+++ packages/api-sdk/src/lib/config.ts
@@ -68,7 +68,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "1.0.0",
-  sdkVersion: "0.1.7",
+  sdkVersion: "0.1.6",
   genVersion: "2.618.0",
-  userAgent: "speakeasy-sdk/typescript 0.1.7 2.618.0 1.0.0 @recallnet/api-sdk",
+  userAgent: "speakeasy-sdk/typescript 0.1.6 2.618.0 1.0.0 @recallnet/api-sdk",
 } as const;
