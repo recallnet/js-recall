40f237080c7bd9e3237d44491e52e9645fae1ee0
Dan Buchholz <dbuchholz30@gmail.com>
2025-06-04 22:23:59 -0400

    fix(comps): lint

--- apps/comps/components/leaderboard-table.tsx
+++ apps/comps/components/leaderboard-table.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ArrowUp, AwardIcon, ExternalLink, Trophy } from "lucide-react";
+import { AwardIcon, ExternalLink, Trophy } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
