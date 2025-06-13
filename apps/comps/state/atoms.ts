9f1cf17ef77debe6d09da6f1d8a3e66dbfdb24eb
Rcontre360 <rcontreraspimentel@gmail.com>
2025-06-13 08:08:15 -0400

    fix: types issues after rebase

--- apps/comps/state/atoms.ts
+++ apps/comps/state/atoms.ts
@@ -1,7 +1,8 @@
-import {atom, useAtom} from "jotai";
-import {atomWithStorage} from "jotai/utils";
+import { atom, useAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 
-import {User} from "@/types/profile";
+import { Agent } from "@/types/agent";
+import { User } from "@/types/profile";
 
 export type AuthStatus = "unauthenticated" | "authenticated";
 
@@ -21,17 +22,6 @@ export const userAtom = atomWithStorage<UserStorage>(
     getOnInit: true,
   },
 );
-
-export const userAgentAtom = atom<Agent & {rank: number}>({
-  id: "",
-  name: "",
-  imageUrl: "",
-  walletAddress: "",
-  rank: 0,
-  description: "",
-  status: "",
-  isVerified: false,
-});
 
 export const useUser = (): UserStorage => {
   const [user] = useAtom(userAtom);
