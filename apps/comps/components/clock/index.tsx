0b0452f4ab0148389119097c1d0dfe7ce8404f28
Joe Wagner <njwjs722@gmail.com>
2025-06-18 14:18:45 -0600

    refactor clock duration logic

--- apps/comps/components/clock/index.tsx
+++ apps/comps/components/clock/index.tsx
@@ -28,39 +28,42 @@ export const CountdownClock: React.FC<CountdownClockProps> = ({
       const now = new Date();
       const difference = targetDate.getTime() - now.getTime();
 
-      if (difference > 0) {
-        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-        if (showDuration === true) {
-          if (days > 1) {
-            if (days < 7) {
-              setDuration(`${days} ${pluralize(days, "day")}`);
-            } else if (days < 30) {
-              const weeks = Math.floor(difference / (1000 * 60 * 60 * 24 * 7));
-              setDuration(`${weeks} ${pluralize(weeks, "week")}`);
-            } else {
-              const months = Math.floor(
-                difference / (1000 * 60 * 60 * 24 * 7 * 30),
-              );
-              setDuration(`${months} ${pluralize(months, "month")}`);
-            }
-
-            return;
-          } else {
-            setDuration("");
-          }
-        }
-        const hours = Math.floor(difference / (1000 * 60 * 60));
-        const minutes = Math.floor(
-          (difference % (1000 * 60 * 60)) / (1000 * 60),
-        );
-        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-        setTimeLeft({ hours, minutes, seconds });
-      } else {
+      if (difference <= 0) {
+        // Countdown is finished, so we will reset time and duration, and
+        // show finished text.
         setTimeLeft({ hours: 0, minutes: 0, seconds: 0 });
         setIsCountdownFinished(true);
         setDuration("");
+        return;
       }
+
+      // We need to test if there is more than 24 hours until start, so we
+      // round down and make sure there is at least one full day
+      const hasDays = Math.floor(difference / (1000 * 60 * 60 * 24)) >= 1;
+      if (hasDays && showDuration === true) {
+        // For display, we want to round to nearest int as you would expect
+        const days = Math.round(difference / (1000 * 60 * 60 * 24));
+        if (days <= 7) {
+          setDuration(`${days} ${pluralize(days, "day")}`);
+        } else if (days < 30) {
+          const weeks = Math.round(difference / (1000 * 60 * 60 * 24 * 7));
+          setDuration(`${weeks} ${pluralize(weeks, "week")}`);
+        } else {
+          const months = Math.round(
+            difference / (1000 * 60 * 60 * 24 * 7 * 30),
+          );
+          setDuration(`${months} ${pluralize(months, "month")}`);
+        }
+
+        return;
+      }
+      const hours = Math.floor(difference / (1000 * 60 * 60));
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+      // reset duration
+      setDuration("");
+      setTimeLeft({ hours, minutes, seconds });
     };
 
     updateCountdown();
