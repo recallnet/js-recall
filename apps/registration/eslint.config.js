149ab16de9c00176c3bf315e29887cfb67036e77
Mark Krasner <mzkrasner@gmail.com>
2025-06-25 15:05:33 -0400

    fix: remove nextauth from layout and edit eslint config

--- apps/registration/eslint.config.js
+++ apps/registration/eslint.config.js
@@ -1,4 +1,12 @@
 import { nextJsConfig } from "@recallnet/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default [
+  ...nextJsConfig,
+  {
+    rules: {
+      // Disable Next.js img element warnings
+      "@next/next/no-img-element": "off",
+    },
+  },
+];
