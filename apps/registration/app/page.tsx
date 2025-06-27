ae882a563527c82c1723fc2e515079f81b72e527
Mark Krasner <mzkrasner@gmail.com>
2025-06-27 12:15:58 -0400

    fix: update initial state on main page component

--- apps/registration/app/page.tsx
+++ apps/registration/app/page.tsx
@@ -33,7 +33,6 @@ export default function Home() {
     name: "",
     email: "",
     website: "",
-    description: "",
   });
   const [agentData, setAgentData] = useState<AgentFormData>({
     name: "",
