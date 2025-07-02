83eac1eeb2bb66735a85089e8e2e66e06eadf140
Rcontre360 <rcontreraspimentel@gmail.com>
2025-07-02 19:54:56 -0400

    fix: rainbow text fix

--- apps/comps/components/animations/rainbow-text.tsx
+++ apps/comps/components/animations/rainbow-text.tsx
@@ -1,4 +1,10 @@
-const RainbowText = ({ text, className = "" }) => {
+const RainbowText = ({
+  text,
+  className = "",
+}: {
+  text: string;
+  className: string;
+}) => {
   return (
     <span
       className={`bg-gradient-to-r from-red-500 via-blue-500 via-green-500 via-orange-500 via-yellow-500 to-purple-500 bg-clip-text text-transparent ${className}`}
