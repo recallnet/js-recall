e4caae60849cf87d4a77cbab5c16a8f1e912cc0f
Rcontre360 <rcontreraspimentel@gmail.com>
2025-07-15 23:03:08 -0400

    feat: first componetn with locked

--- apps/comps/components/agent-profile/credentials.tsx
+++ apps/comps/components/agent-profile/credentials.tsx
@@ -1,15 +1,19 @@
-import { KeyRound } from "lucide-react";
-import { useState } from "react";
+import {KeyRound, Mail} from "lucide-react";
+import {useState} from "react";
 
-import { Tooltip } from "@recallnet/ui2/components/tooltip";
-import { cn } from "@recallnet/ui2/lib/utils";
+import {Tooltip} from "@recallnet/ui2/components/tooltip";
+import {toast} from "@recallnet/ui2/components/toast";
+import {Button} from "@recallnet/ui2/components/button";
+import {cn} from "@recallnet/ui2/lib/utils";
 
-import { useApiKey } from "@/hooks/useApiKey";
-import { useSandboxAgentApiKey } from "@/hooks/useSandbox";
-import { Agent } from "@/types";
+import {useApiKey} from "@/hooks/useApiKey";
+import {useSandboxAgentApiKey} from "@/hooks/useSandbox";
+import {Agent} from "@/types";
 
-import { CopyButton } from "../copy-button";
-import { VisibilityToggle } from "../visibility-toggle";
+import {CopyButton} from "../copy-button";
+import {VisibilityToggle} from "../visibility-toggle";
+import {useProfile} from "@/hooks/useProfile";
+import {useVerifyEmail} from "@/hooks/useVerifyEmail";
 
 /**
  * Component for displaying an API key row with visibility toggle and copy functionality
@@ -53,6 +57,83 @@ const ApiKeyRow = ({
   );
 };
 
+const ApiKeyLocked = () => {
+  const {mutate: verifyEmail, isPending} = useVerifyEmail();
+  const [emailVerifyClicked, setEmailVerifyClicked] = useState(false);
+
+  const onSendEmail = async () => {
+    verifyEmail(undefined, {
+      onSuccess: (res) => {
+        if (res.success) {
+          toast.success(
+            <div className="flex flex-col">
+              <span>Verification Email Sent</span>
+              <span className="text-primary-foreground font-normal">
+                An email has been sent to your inbox.
+              </span>
+            </div>,
+          );
+          setTimeout(setEmailVerifyClicked, 60 * 1000, false); //wait 60 seconds
+        } else {
+          toast.error(res.message);
+        }
+      },
+      onError: (res) => {
+        toast.error("Failed to send verification email", {
+          description: res.message,
+        });
+      },
+    });
+  }
+
+  return (
+    <div className="w-full grid grid-cols-1 sm:grid-cols-[30px_200px_1fr_300px] gap-4 items-center">
+      <div className="flex items-center justify-center w-8 mx-auto md:mx-0">
+        <KeyRound className="w-7 h-7 text-gray-500" strokeWidth={1.3} />
+      </div>
+
+      <div className="text-center md:text-left">
+        <span className="text-sm font-bold block text-secondary-foreground">
+          Your API keys are
+          <span className="text-red-300 ml-1">locked</span>
+          .
+        </span>
+      </div>
+
+      <div className="text-center md:text-left">
+        <span className="text-sm text-secondary-foreground">
+          To access Production and Sandbox API keys, verify your email.
+          Once verified, you can unlock them.
+        </span>
+      </div>
+
+      <div className="flex justify-center md:justify-end">
+        {emailVerifyClicked ? (
+          <Tooltip content='You can only send a verification each 60 seconds'>
+            <Button
+              onClick={onSendEmail}
+              disabled={isPending || emailVerifyClicked}
+            >
+              <Mail className="w-6 h-6" strokeWidth={1.3} />
+              <span>Verify EMAIL</span>
+            </Button>
+          </Tooltip>
+        ) : (
+          <Button
+            onClick={onSendEmail}
+            disabled={isPending || emailVerifyClicked}
+            className='bg-blue-600 py-7 px-12 text-xs flex gap-3'
+          >
+            <Mail className="w-6 h-6" strokeWidth={1.3} />
+            <span>Verify EMAIL</span>
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+
 export const Credentials = ({
   agent,
   className,
@@ -60,8 +141,9 @@ export const Credentials = ({
   agent: Agent;
   className?: string;
 }) => {
-  const { data: apiKey, isLoading } = useApiKey(agent.id);
-  const { data: sandboxApiKey, isLoading: sandboxLoading } =
+  const {isEmailVerified} = useProfile()
+  const {data: apiKey, isLoading} = useApiKey(agent.id);
+  const {data: sandboxApiKey, isLoading: sandboxLoading} =
     useSandboxAgentApiKey(agent.name);
 
   return (
@@ -71,20 +153,26 @@ export const Credentials = ({
         className,
       )}
     >
-      <ApiKeyRow
-        label="Production API Key"
-        tooltip="Agent API Key"
-        apiKey={apiKey?.apiKey}
-        isLoading={isLoading}
-      />
-      {sandboxApiKey && (
-        <ApiKeyRow
-          label="Sandbox API Key"
-          tooltip="Sandbox Agent API Key"
-          apiKey={sandboxApiKey?.agent.apiKey}
-          isLoading={sandboxLoading}
-        />
-      )}
+      {isEmailVerified ?
+        <>
+          <ApiKeyRow
+            label="Production API Key"
+            tooltip="Agent API Key"
+            apiKey={apiKey?.apiKey}
+            isLoading={isLoading}
+          />
+          {sandboxApiKey && (
+            <ApiKeyRow
+              label="Sandbox API Key"
+              tooltip="Sandbox Agent API Key"
+              apiKey={sandboxApiKey?.agent.apiKey}
+              isLoading={sandboxLoading}
+            />
+          )}
+        </>
+        :
+        <ApiKeyLocked />
+      }
     </div>
   );
 };
