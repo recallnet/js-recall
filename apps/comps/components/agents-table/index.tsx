c083bca9cebeb7bd265edcd4be143720c7efe0fd
juampibermani <juampibermani@gmail.com>
2025-07-22 17:04:04 +0000

    Remove max height

--- apps/comps/components/agents-table/index.tsx
+++ apps/comps/components/agents-table/index.tsx
@@ -314,7 +314,6 @@ export const AgentsTable: React.FC<AgentsTableProps> = ({
       <div
         ref={tableContainerRef}
         style={{
-          maxHeight: "680px", // 10 rows × 68px height
           overflowY: "auto",
           position: "relative",
         }}
