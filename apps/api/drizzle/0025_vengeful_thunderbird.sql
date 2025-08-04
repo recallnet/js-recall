2a9943649b8fac517596dd0610f6ba1873250818
Dan Buchholz <dbuchholz30@gmail.com>
2025-08-04 11:49:06 -0400

    fix(api): remove bad migration file

--- apps/api/drizzle/0025_vengeful_thunderbird.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "agents" ADD COLUMN "handle" varchar(50);--> statement-breakpoint
-CREATE INDEX "idx_agents_handle" ON "agents" USING btree ("handle");--> statement-breakpoint
-ALTER TABLE "agents" ADD CONSTRAINT "agents_handle_key" UNIQUE("handle");
