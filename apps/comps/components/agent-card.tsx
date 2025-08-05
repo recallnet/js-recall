8bf5ec3f4de672091d42679b03b91b367808238b
Dan Buchholz <dbuchholz30@gmail.com>
2025-08-05 18:18:46 -0400

    feat(comps): agent handle

--- apps/comps/components/agent-card.tsx
+++ apps/comps/components/agent-card.tsx
@@ -13,7 +13,9 @@ interface AgentCardProps {
 export const AgentCard: React.FC<AgentCardProps> = ({ agent }) => {
   return (
     <div className="bg-card flex h-52 items-end justify-between p-6">
-      <h3 className="text-primary text-lg font-bold">{agent.name}</h3>
+      <h3 className="text-primary text-lg font-bold">
+        {agent.name} (@{agent.handle})
+      </h3>
       <Link href={`/agents/${agent.id}`}>
         <button
           className="rounded-full p-1.5 hover:bg-slate-700"
