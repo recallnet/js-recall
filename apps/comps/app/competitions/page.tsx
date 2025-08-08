3fa9fb4d98d54f445ce4d616cc709ca0116c5065
Rcontre360 <rcontreraspimentel@gmail.com>
2025-08-08 18:39:32 -0400

    feat: removed unused assets. Added rainbow elements and hero section

--- apps/comps/app/competitions/page.tsx
+++ apps/comps/app/competitions/page.tsx
@@ -1,30 +1,31 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 
-import { cn } from "@recallnet/ui2/lib/utils";
+import {cn} from "@recallnet/ui2/lib/utils";
 
-import { CompetitionsCollapsible } from "@/components/competitions-collapsible";
+import {CompetitionsCollapsible} from "@/components/competitions-collapsible";
 import CompetitionsSkeleton from "@/components/competitions-skeleton";
-import { FeaturedCompetition } from "@/components/featured-competition";
-import { FooterSection } from "@/components/footer-section";
-import { JoinSwarmSection } from "@/components/join-swarm-section";
-import { getSocialLinksArray } from "@/data/social";
-import { useCompetitions, useUserCompetitions } from "@/hooks/useCompetitions";
-import { useAnalytics } from "@/hooks/usePostHog";
-import { CompetitionStatus } from "@/types";
-import { mergeCompetitionsWithUserData } from "@/utils/competition-utils";
+import {FeaturedCompetition} from "@/components/featured-competition";
+import {FooterSection} from "@/components/footer-section";
+import {JoinSwarmSection} from "@/components/join-swarm-section";
+import {getSocialLinksArray} from "@/data/social";
+import {useCompetitions, useUserCompetitions} from "@/hooks/useCompetitions";
+import {useAnalytics} from "@/hooks/usePostHog";
+import {CompetitionStatus} from "@/types";
+import {mergeCompetitionsWithUserData} from "@/utils/competition-utils";
+import {Button} from "@/../../packages/ui2/src/components/button";
 
 export default function CompetitionsPage() {
-  const { trackEvent } = useAnalytics();
+  const {trackEvent} = useAnalytics();
 
   // Track landing page view
   useEffect(() => {
     trackEvent("LandingPageViewed");
   }, [trackEvent]);
 
-  const { data: activeCompetitions, isLoading: isLoadingActiveCompetitions } =
+  const {data: activeCompetitions, isLoading: isLoadingActiveCompetitions} =
     useCompetitions({
       status: CompetitionStatus.Active,
     });
@@ -36,12 +37,12 @@ export default function CompetitionsPage() {
     status: CompetitionStatus.Pending,
   });
 
-  const { data: endedCompetitions, isLoading: isLoadingEndedCompetitions } =
+  const {data: endedCompetitions, isLoading: isLoadingEndedCompetitions} =
     useCompetitions({
       status: CompetitionStatus.Ended,
     });
 
-  const { data: userCompetitions, isLoading: isLoadingUserCompetitions } =
+  const {data: userCompetitions, isLoading: isLoadingUserCompetitions} =
     useUserCompetitions();
 
   if (
@@ -53,52 +54,38 @@ export default function CompetitionsPage() {
     return <CompetitionsSkeleton />;
   }
 
-  const featuredCompetition =
-    activeCompetitions?.competitions?.[0] ||
-    upcomingCompetitions?.competitions?.[0];
-
-  const featuredCompetitionWithAgents = featuredCompetition
-    ? mergeCompetitionsWithUserData(
-        [featuredCompetition],
-        userCompetitions?.competitions ?? [],
-      )[0]
-    : null;
-
   return (
-    <div className="relative">
-      {featuredCompetitionWithAgents && (
-        <div className="absolute z-0 h-[814px] w-full sm:hidden">
-          <Video />
-        </div>
-      )}
+    <div >
+      <div className="w-full h-100 absolute left-1/2 transform -translate-x-1/2 relative ">
 
-      <div className="sm:min-h-[85vh]">
-        <div className="relative flex h-full flex-col gap-8 sm:min-h-[85vh] sm:flex-row">
-          <div className="absolute z-0 h-full w-full">
-            <Video />
-            <div className="relative h-1/2 w-full bg-[radial-gradient(ellipse_at_center,_transparent_40%,_black_80%)] sm:h-full sm:bg-[radial-gradient(ellipse_at_center,_transparent_30%,_black_90%)]" />
-          </div>
-          <div className="z-10 mb-10 flex w-full flex-col items-center justify-between gap-8">
-            <div className="mt-30 sm:mt-15 flex max-w-[434px] flex-col items-center gap-2">
-              <span className="text-primary-foreground text-center text-7xl font-bold">
-                Join. Vote.
-              </span>
-              <span className="text-primary-foreground text-9xl font-bold">
-                Win.
-              </span>
-            </div>
-            <p className="text-primary-foreground max-w-[434px] text-center">
-              Register your AI agents, place your votes, and watch the action
-              unfold. At Recall, every competition is powered by smart agents
-              trying to beat the crypto market.{" "}
-              <span className="font-bold">
-                Join now and turn your insight into real rewards.
-              </span>
+        <div className="flex items-center justify-center w-full h-full">
+
+          {
+            <RainbowStripes className="w-180 absolute left-0 translate-x-[-400px]" />
+          }
+
+          <div className="flex flex-col items-center text-center translate-y-[-50px]">
+            <h1 className="text-[83px] font-bold text-primary-foreground mb-1">
+              Enter the Arena
+            </h1>
+            <p className="text-sm text-primary-foreground mb-8">
+              Stake tokens, back the smartest trading bots and earn rewards.
             </p>
+
+            <div className="flex gap-1">
+              <Button className="p-6 bg-white text-black border border-white hover:bg-black hover:text-white transition-colors duration-200">
+                BROWSE COMPETITIONS
+              </Button>
+              <Button className="p-6 bg-black text-white border border-white hover:bg-white hover:text-black transition-colors duration-200">
+                JOIN
+              </Button>
+            </div>
           </div>
-          {featuredCompetitionWithAgents && (
-            <FeaturedCompetition competition={featuredCompetitionWithAgents} />
-          )}
+
+          {
+            <RainbowStripes className="w-180 absolute right-0 translate-x-[400px]" direction='left' />
+          }
+
         </div>
       </div>
 
@@ -137,31 +124,56 @@ export default function CompetitionsPage() {
   );
 }
 
-const Video: React.FC<{ className?: string }> = ({ className }) => {
+
+
+interface RainbowStripesProps {
+  direction?: 'left' | 'right';
+  width?: string;
+  height?: string;
+  className?: string;
+}
+
+export const RainbowStripes: React.FC<RainbowStripesProps> = ({
+  direction = 'right',
+  className = ''
+}) => {
+  const colors = [
+    '#FF0000', // Red
+    '#FFA500', // Orange/Yellow
+    '#00FF00', // Green
+    '#0000FF', // Blue
+    '#000000'  // Black
+  ];
+
+  // Create the clip-path for the triangular spike
+  const clipPath = direction === 'right'
+    ? 'polygon(0 0, calc(100% - 140px) 0, 100% 80%, 100% 100%, 0 100%)'
+    : 'polygon(calc(0% + 140px) 0, 100% 0, 100% 100%, 0% 100%, 0% 80% )';
+
   return (
-    <video
-      autoPlay
-      muted
-      loop
-      playsInline
-      className={cn(
-        "absolute left-0 top-0 h-full w-full object-cover",
-        className,
-      )}
-      poster="/video-placeholder.png"
+    <div
+      className={className}
     >
-      <source
-        src="https://5pskttgrmgbdllus.public.blob.vercel-storage.com/competitions_hub-Ki6pZVJaHivsu3ZeAqzUtlGWjjdwcS.mp4"
-        type="video/mp4"
-      />
-      <Image
-        src="/video-placeholder.png"
-        alt="Video placeholder"
-        fill
-        sizes="(max-width: 640px) 100vw, 100vw"
-        className="object-cover"
-        priority
-      />
-    </video>
+      <div
+        className="relative h-60"
+      >
+        <div className={cn(
+          `absolute w-full top-0 bottom-0 z-10`,
+          `bg-gradient-to-${direction === 'right' ? 'r' : 'l'} from-black from-10% to-transparent`,
+        )}></div>
+        {colors.map((color, index) => (
+          <div
+            key={index}
+            className={cn(`absolute w-full h-45`)}
+            style={{
+              backgroundColor: color,
+              clipPath: clipPath,
+              top: `calc(38px * ${index})`,
+              transform: `translateX(${index * 30 * (direction === 'right' ? 1 : -1)}px)`
+            }}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
