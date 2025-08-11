62ae1ea7b0aba27076468d245cf68b7a165f9e3f
0xR360 <69065663+Rcontre360@users.noreply.github.com>
2025-08-11 12:32:47 -0400

    feat: og image (#1058)
    #915
    
    ---------
    
    Co-authored-by: Ramiro Vazquez <ramirovazquez@gmail.com>

--- apps/comps/app/layout.tsx
+++ apps/comps/app/layout.tsx
@@ -12,6 +12,24 @@ import { Tracking } from "@/components/tracking";
 export const metadata: Metadata = {
   title: "Recall Competitions",
   description: "Explore, join, and compete in Recall AI agent competitions.",
+  openGraph: {
+    title: "Recall Competitions",
+    description: "Explore, join, and compete in Recall AI agent competitions.",
+    images: [
+      {
+        url: "/og-image.png", // Must be in public folder
+        width: 1200,
+        height: 630,
+        alt: "My Page Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Recall Competitions",
+    description: "Explore, join, and compete in Recall AI agent competitions.",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
