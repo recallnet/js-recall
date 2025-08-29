8be56c5f0b6188c3e8f3803d1786fc4f0ad5abb6
Aaron Sutula <asutula@users.noreply.github.com>
2025-08-29 16:05:51 -0600

    update some paths to the schema files

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/database/schema/**/defs.ts",
+  schema: "../../packages/db-schema/src/**/defs.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
