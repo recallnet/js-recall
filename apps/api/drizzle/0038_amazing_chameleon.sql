76fb7aa919dae0b486d8ce44ba814832eef62549
Sergey Ukustov <sergey@ukstv.me>
2025-08-29 17:21:35 +0300

    feat: migrations

---
+++ apps/api/drizzle/0038_amazing_chameleon.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "stake_changes" ALTER COLUMN "wallet" SET DATA TYPE varchar(42);--> statement-breakpoint
+ALTER TABLE "boost_balances" ALTER COLUMN "wallet" SET DATA TYPE varchar(42);--> statement-breakpoint
+ALTER TABLE "boost_changes" ALTER COLUMN "wallet" SET DATA TYPE varchar(42);
