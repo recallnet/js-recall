4dc02b8b5188b6bd74e9630e5bb9880adbf24572
Aaron Sutula <asutula@users.noreply.github.com>
2025-08-29 16:21:22 -0600

    updaate tsup config

--- packages/db-schema/tsup.config.ts
+++ packages/db-schema/tsup.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: [
+    "src/index.ts",
+    "src/core/defs.ts",
+    "src/core/relatons.ts",
+    "src/core/types.ts",
+    "src/ranking/defs.ts",
+    "src/ranking/relatons.ts",
+    "src/ranking/types.ts",
+    "src/trading/defs.ts",
+    "src/trading/relatons.ts",
+    "src/trading/types.ts",
+    "src/voting/defs.ts",
+    "src/voting/relatons.ts",
+    "src/voting/types.ts",
+  ],
   format: ["cjs", "esm"],
   dts: true,
   clean: true,
