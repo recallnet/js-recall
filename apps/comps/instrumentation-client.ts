cf92535401a4bedb3730a0169026c590552f0bca
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2025-09-02 16:01:45 -0400

    fix: build failures/warnings

--- apps/comps/instrumentation-client.ts
+++ apps/comps/instrumentation-client.ts
@@ -1,3 +1,6 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
@@ -22,3 +25,5 @@ Sentry.init({
     }),
   ],
 });
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
