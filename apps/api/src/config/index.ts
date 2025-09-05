0162d0fb984e43394ab9461b82c119b3a413a27c
Mohsin Zaidi <2236875+smrz2001@users.noreply.github.com>
2025-09-05 11:24:29 -0400

    rebase

--- apps/api/src/config/index.ts
+++ apps/api/src/config/index.ts
@@ -359,7 +359,7 @@ export const config = {
       },
     },
   },
-stakingIndex: {
+  stakingIndex: {
     isEnabled: process.env.INDEXING_ENABLED === "true",
     recallContract: process.env.INDEXING_STAKING_CONTRACT,
     startBlock: process.env.INDEXING_START_BLOCK
