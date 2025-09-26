7cebd92027889705f2dc59a1da369ba1a7a0248c
Aaron Sutula <asutula@users.noreply.github.com>
2025-09-26 13:19:17 -0600

    remove boost to compete wording

--- apps/comps/app/competitions/[id]/page.tsx
+++ apps/comps/app/competitions/[id]/page.tsx
@@ -33,7 +33,6 @@ import { useSession } from "@/hooks/useSession";
 const LIMIT_AGENTS_PER_PAGE = 10;
 const LIMIT_TRADES_PER_PAGE = 10;
 const LIMIT_POSITIONS_PER_PAGE = 10;
-const COST_TO_COMPETE = 300;
 
 export default function CompetitionPage({
   params,
@@ -197,12 +196,7 @@ export default function CompetitionPage({
               disabled={competition.status !== "pending"}
               size="lg"
             >
-              <span>
-                COMPETE{" "}
-                <span className="text-yellow-500">{COST_TO_COMPETE}</span>{" "}
-                <span className="font-bold">Boost</span>{" "}
-                <Zap className="inline h-4 w-4 text-yellow-500" />
-              </span>{" "}
+              <span>COMPETE</span>{" "}
             </JoinCompetitionButton>
 
             <BoostAgentsBtn className="w-full justify-between uppercase sm:w-1/2" />
