e1c21459b29b2ba568f279bb9f9c1dd8bcd2f81a
Dan Buchholz <dbuchholz30@gmail.com>
2025-09-28 13:37:39 -0400

    fix(comps): use ssl if cert available (#1301)
    closes
    [APP-514](https://linear.app/recall-labs/issue/APP-514/bug-comps-postres-connection-is-missing-cert)

--- apps/comps/lib/db.ts
+++ apps/comps/lib/db.ts
@@ -1,5 +1,25 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 import schema from "@recallnet/db/schema";
 
-export const db = drizzle(process.env.POSTGRES_URL || "", { schema });
+const ca = process.env.DATABASE_CA_CERT
+  ? Buffer.from(process.env.DATABASE_CA_CERT, "base64").toString("utf8")
+  : undefined;
+
+const sslConfig = () => {
+  if (ca) {
+    return {
+      ca,
+      rejectUnauthorized: true,
+    };
+  }
+  return false;
+};
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+  ssl: sslConfig(),
+});
+
+export const db = drizzle(pool, { schema });
