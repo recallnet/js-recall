ef45f45e5f69e8c420658cfa1844e11942b2de3b
Aaron Sutula <asutula@users.noreply.github.com>
2025-09-30 23:13:32 -0600

    coverage testing for more packages

--- packages/services/vitest.config.ts
+++ packages/services/vitest.config.ts
@@ -1,5 +1,5 @@
 import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
@@ -8,14 +8,8 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "json-summary"],
-      exclude: [
-        "node_modules/**",
-        "dist/**",
-        "**/*.d.ts",
-        "**/*.test.ts",
-        "**/*.spec.ts",
-        "**/test/**",
-      ],
+      include: ["src/**/*.ts"],
+      exclude: [...coverageConfigDefaults.exclude],
     },
     projects: [
       {
