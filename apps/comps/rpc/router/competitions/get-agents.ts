e5d60e4e9c0fa40b7d833f403feaf38a6f2ac4b6
Spencer T Brody <spencer+github@textile.io>
2025-10-06 21:45:28 -0400

    chore: Update ORPCError imports to get from server package

--- apps/comps/rpc/router/competitions/get-agents.ts
+++ apps/comps/rpc/router/competitions/get-agents.ts
@@ -1,4 +1,4 @@
-import { ORPCError } from "@orpc/client";
+import { ORPCError } from "@orpc/server";
 import { z } from "zod/v4";
 
 import { ApiError, PagingParamsSchema } from "@recallnet/services/types";
