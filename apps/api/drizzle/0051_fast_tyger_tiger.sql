40aefb6a82e59e7ad7853e5ee0138b90376d70e6
Dan Buchholz <dbuchholz30@gmail.com>
2025-10-13 17:50:09 -0400

    fix(api): post-rebase sanctions db migrations

--- apps/api/drizzle/0051_fast_tyger_tiger.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "sanctioned_wallets" (
-	"address" varchar(42) PRIMARY KEY NOT NULL,
-	"created_at" timestamp with time zone DEFAULT now() NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "competitions" DROP COLUMN "minimum_stake";--> statement-breakpoint
-ALTER TABLE "stake_boost_awards" DROP COLUMN "base_amount";--> statement-breakpoint
-ALTER TABLE "stake_boost_awards" DROP COLUMN "multiplier";
