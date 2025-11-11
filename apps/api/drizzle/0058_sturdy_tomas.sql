e83d8c08279da65881cbb313f8573d8a87cfd1e9
Mark Krasner <mzkrasner@gmail.com>
2025-11-11 16:14:58 -0500

    chore: temp removal of schema migration files

--- apps/api/drizzle/0058_sturdy_tomas.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "agents" ADD COLUMN "is_rewards_ineligible" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "agents" ADD COLUMN "rewards_ineligibility_reason" text;--> statement-breakpoint
-ALTER TABLE "competitions" ADD COLUMN "rewards_ineligible" text[];--> statement-breakpoint
-CREATE INDEX "idx_agents_rewards_ineligible" ON "agents" USING btree ("is_rewards_ineligible");
