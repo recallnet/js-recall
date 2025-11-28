f1a35524d8e79e130266841faaa5b7dfc632c3de
Carson Farmer <carson.farmer@gmail.com>
2025-11-28 09:46:39 -0800

    wip: style cleanups
    Signed-off-by: Carson Farmer <carson.farmer@gmail.com>

--- apps/comps/app/competitions/[id]/og-image/route.tsx
+++ apps/comps/app/competitions/[id]/og-image/route.tsx
@@ -39,13 +39,7 @@ export async function GET(
   if (!competition) {
     return new ImageResponse(
       (
-        <div
-          tw="flex w-full h-full items-center justify-center bg-[#020817] text-white text-[54px] font-bold"
-          style={{
-            fontFamily:
-              "system-ui, -apple-system, BlinkMacSystemFont, sans-serif",
-          }}
-        >
+        <div tw="flex w-full h-full items-center justify-center bg-slate-950 text-white text-5xl font-bold">
           Recall Competitions
         </div>
       ),
@@ -62,19 +56,20 @@ export async function GET(
   return new ImageResponse(
     (
       <div
-        tw="flex flex-col items-center justify-center w-full h-full bg-no-repeat bg-[length:100%_100%]"
+        tw="flex flex-col items-center justify-center w-full h-full"
         style={{
           backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "100% 100%",
         }}
       >
-        <div tw="flex flex-col items-center justify-center gap-12 w-[916px]">
-          <div tw="font-bold text-[72px] text-[#e5e5e5] leading-none text-center w-[914px]">
+        <div tw="flex flex-col items-center justify-center gap-12 w-3/4">
+          <h1 tw="font-bold text-7xl text-neutral-200 leading-none text-center flex justify-center">
             {competition.name}
-          </div>
+          </h1>
 
-          <div tw="flex flex-row gap-3 w-[916px] h-[212px]">
-            <div tw="flex flex-col gap-3 w-[452px] h-[212px]">
-              <div tw="flex flex-row justify-center items-center gap-3 w-[451px] h-[72px] bg-black/40 border border-[#3D3D3D] rounded-lg">
+          <div tw="flex flex-row gap-3 w-full">
+            <div tw="flex flex-col gap-3 flex-1">
+              <div tw="flex flex-row justify-center items-center gap-3 py-4 bg-black/40 border border-neutral-700 rounded-lg">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                   src={recallTokenSvg}
@@ -82,20 +77,20 @@ export async function GET(
                   width={24}
                   height={24}
                 />
-                <span tw="font-normal text-[40px] text-[#E5E5E5] leading-none">
+                <span tw="text-4xl text-neutral-200 leading-none">
                   {formatBigintAmount(
                     BigInt(competition.rewardsTge?.agentPool ?? 0),
                   )}
-                </span>
-                <span tw="font-light text-[32px] text-[#A7A7A7] leading-none">
+                </span>{" "}
+                <span tw="font-light text-3xl text-neutral-400 leading-none">
                   for Agents
                 </span>
               </div>
-              <div tw="flex flex-col justify-center items-center gap-3 w-[452px] h-[128px] bg-black/40 border border-[#3D3D3D] rounded-lg py-4 px-3">
-                <span tw="text-[32px] font-normal text-[#A7A7A7] leading-none">
+              <div tw="flex flex-col justify-center items-center gap-3 py-6 bg-black/40 border border-neutral-700 rounded-lg">
+                <span tw="text-3xl text-neutral-400 leading-none">
                   Duration
                 </span>
-                <span tw="text-[40px] font-normal text-[#E5E5E5] leading-none">
+                <span tw="text-4xl text-neutral-200 leading-none">
                   {formatCompetitionDates(
                     competition.startDate,
                     competition.endDate,
@@ -104,8 +99,8 @@ export async function GET(
               </div>
             </div>
 
-            <div tw="flex flex-col gap-3 w-[452px] h-[212px]">
-              <div tw="flex flex-row justify-center items-center gap-3 w-[451px] h-[72px] bg-black/40 border border-[#3D3D3D] rounded-lg">
+            <div tw="flex flex-col gap-3 flex-1">
+              <div tw="flex flex-row justify-center items-center gap-3 py-4 bg-black/40 border border-neutral-700 rounded-lg">
                 {/* eslint-disable-next-line @next/next/no-img-element */}
                 <img
                   src={recallTokenSvg}
@@ -113,20 +108,20 @@ export async function GET(
                   width={24}
                   height={24}
                 />
-                <span tw="font-normal text-[40px] text-[#E5E5E5] leading-none">
+                <span tw="text-4xl text-neutral-200 leading-none">
                   {formatBigintAmount(
                     BigInt(competition.rewardsTge?.userPool ?? 0),
                   )}
-                </span>
-                <span tw="font-light text-[32px] text-[#A7A7A7] leading-none">
+                </span>{" "}
+                <span tw="font-light text-3xl text-neutral-400 leading-none">
                   for Boosters
                 </span>
               </div>
-              <div tw="flex flex-col justify-center items-center gap-3 w-[452px] h-[128px] bg-black/40 border border-[#3D3D3D] rounded-lg py-4 px-3">
-                <span tw="text-[32px] font-normal text-[#A7A7A7] leading-none">
+              <div tw="flex flex-col justify-center items-center gap-3 py-6 bg-black/40 border border-neutral-700 rounded-lg">
+                <span tw="text-3xl text-neutral-400 leading-none">
                   Boost Window
                 </span>
-                <span tw="text-[40px] font-normal text-[#E5E5E5] leading-none">
+                <span tw="text-4xl text-neutral-200 leading-none">
                   {formatCompetitionDates(
                     competition.boostStartDate,
                     competition.boostEndDate,
@@ -136,10 +131,10 @@ export async function GET(
             </div>
           </div>
 
-          <div tw="flex flex-col items-center gap-4 w-full">
+          <div tw="flex flex-row items-center gap-4 justify-center">
             {/* eslint-disable-next-line @next/next/no-img-element */}
             <img src={recallLogoSvg} alt="Recall logo" height={32} />
-            <div tw="font-normal text-[32px] text-[#87B0D9] tracking-wider leading-none font-mono">
+            <div tw="text-3xl text-sky-300 tracking-wider leading-none font-mono">
               https://app.recall.network
             </div>
           </div>
